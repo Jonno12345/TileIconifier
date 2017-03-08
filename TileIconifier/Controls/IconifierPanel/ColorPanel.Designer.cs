c96dbe90b0ca1d586185d9319410e16708be51d4
mcdenis <maxecole@hotmail.com>
2017-03-07 21:24:39 -0500

    More layout improvements (I had forgotten a couple of AutoSize and possibly some Margins) and replaced the pnlFGColour with a TableLayoutPanel.

--- TileIconifier/Controls/IconifierPanel/ColorPanel.Designer.cs
+++ TileIconifier/Controls/IconifierPanel/ColorPanel.Designer.cs
@@ -32,17 +32,17 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.eyedropperColorPicker = new TileIconifier.Controls.Eyedropper.EyedropColorPicker();
             this.btnColourPicker = new TileIconifier.Controls.SkinnableButton();
-            this.pnlFGColour = new System.Windows.Forms.Panel();
+            this.cmbColour = new TileIconifier.Controls.SkinnableComboBox();
+            this.lblBGColour = new System.Windows.Forms.Label();
+            this.txtBGColour = new TileIconifier.Controls.SkinnableTextBox();
             this.chkFGTxtEnabled = new TileIconifier.Controls.SkinnableCheckBox();
             this.radFGDark = new TileIconifier.Controls.SkinnableRadioButton();
             this.lblFGText = new System.Windows.Forms.Label();
             this.radFGLight = new TileIconifier.Controls.SkinnableRadioButton();
-            this.cmbColour = new TileIconifier.Controls.SkinnableComboBox();
-            this.lblBGColour = new System.Windows.Forms.Label();
-            this.txtBGColour = new TileIconifier.Controls.SkinnableTextBox();
             this.clrDialog = new System.Windows.Forms.ColorDialog();
+            this.tlpFGColour = new System.Windows.Forms.TableLayoutPanel();
             this.panel1.SuspendLayout();
-            this.pnlFGColour.SuspendLayout();
+            this.tlpFGColour.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -50,7 +50,6 @@
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Controls.Add(this.eyedropperColorPicker);
             this.panel1.Controls.Add(this.btnColourPicker);
-            this.panel1.Controls.Add(this.pnlFGColour);
             this.panel1.Controls.Add(this.cmbColour);
             this.panel1.Controls.Add(this.lblBGColour);
             this.panel1.Controls.Add(this.txtBGColour);
@@ -66,22 +65,35 @@
             // 
             // btnColourPicker
             // 
-            resources.ApplyResources(this.btnColourPicker, "btnColourPicker");
             this.btnColourPicker.BackgroundImage = global::TileIconifier.Properties.Resources.colorsquare;
+            resources.ApplyResources(this.btnColourPicker, "btnColourPicker");
             this.btnColourPicker.DisabledForeColor = System.Drawing.Color.Empty;
             this.btnColourPicker.FlatAppearance.BorderSize = 0;
             this.btnColourPicker.Name = "btnColourPicker";
             this.btnColourPicker.UseVisualStyleBackColor = true;
             this.btnColourPicker.Click += new System.EventHandler(this.btnColourPicker_Click);
             // 
-            // pnlFGColour
+            // cmbColour
             // 
-            resources.ApplyResources(this.pnlFGColour, "pnlFGColour");
-            this.pnlFGColour.Controls.Add(this.chkFGTxtEnabled);
-            this.pnlFGColour.Controls.Add(this.radFGDark);
-            this.pnlFGColour.Controls.Add(this.lblFGText);
-            this.pnlFGColour.Controls.Add(this.radFGLight);
-            this.pnlFGColour.Name = "pnlFGColour";
+            this.cmbColour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColour.FlatButtonBackColor = System.Drawing.SystemColors.Control;
+            this.cmbColour.FlatButtonBorderColor = System.Drawing.SystemColors.ControlDark;
+            this.cmbColour.FlatButtonBorderFocusedColor = System.Drawing.SystemColors.Highlight;
+            this.cmbColour.FlatButtonDisabledForeColor = System.Drawing.SystemColors.GrayText;
+            this.cmbColour.FlatButtonForeColor = System.Drawing.SystemColors.ControlText;
+            this.cmbColour.FormattingEnabled = true;
+            resources.ApplyResources(this.cmbColour, "cmbColour");
+            this.cmbColour.Name = "cmbColour";
+            // 
+            // lblBGColour
+            // 
+            resources.ApplyResources(this.lblBGColour, "lblBGColour");
+            this.lblBGColour.Name = "lblBGColour";
+            // 
+            // txtBGColour
+            // 
+            resources.ApplyResources(this.txtBGColour, "txtBGColour");
+            this.txtBGColour.Name = "txtBGColour";
             // 
             // chkFGTxtEnabled
             // 
@@ -102,6 +114,7 @@
             // lblFGText
             // 
             resources.ApplyResources(this.lblFGText, "lblFGText");
+            this.tlpFGColour.SetColumnSpan(this.lblFGText, 2);
             this.lblFGText.Name = "lblFGText";
             // 
             // radFGLight
@@ -113,39 +126,27 @@
             this.radFGLight.TabStop = true;
             this.radFGLight.UseVisualStyleBackColor = true;
             // 
-            // cmbColour
+            // tlpFGColour
             // 
-            resources.ApplyResources(this.cmbColour, "cmbColour");
-            this.cmbColour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbColour.FlatButtonBackColor = System.Drawing.SystemColors.Control;
-            this.cmbColour.FlatButtonBorderColor = System.Drawing.SystemColors.ControlDark;
-            this.cmbColour.FlatButtonBorderFocusedColor = System.Drawing.SystemColors.Highlight;
-            this.cmbColour.FlatButtonDisabledForeColor = System.Drawing.SystemColors.GrayText;
-            this.cmbColour.FlatButtonForeColor = System.Drawing.SystemColors.ControlText;
-            this.cmbColour.FormattingEnabled = true;
-            this.cmbColour.Name = "cmbColour";
-            // 
-            // lblBGColour
-            // 
-            resources.ApplyResources(this.lblBGColour, "lblBGColour");
-            this.lblBGColour.Name = "lblBGColour";
-            // 
-            // txtBGColour
-            // 
-            resources.ApplyResources(this.txtBGColour, "txtBGColour");
-            this.txtBGColour.Name = "txtBGColour";
+            resources.ApplyResources(this.tlpFGColour, "tlpFGColour");
+            this.tlpFGColour.Controls.Add(this.lblFGText, 0, 0);
+            this.tlpFGColour.Controls.Add(this.radFGDark, 1, 1);
+            this.tlpFGColour.Controls.Add(this.radFGLight, 0, 1);
+            this.tlpFGColour.Controls.Add(this.chkFGTxtEnabled, 2, 0);
+            this.tlpFGColour.Name = "tlpFGColour";
             // 
             // ColorPanel
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tlpFGColour);
             this.Controls.Add(this.panel1);
             this.Name = "ColorPanel";
             this.Load += new System.EventHandler(this.ColorPanel_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.pnlFGColour.ResumeLayout(false);
-            this.pnlFGColour.PerformLayout();
+            this.tlpFGColour.ResumeLayout(false);
+            this.tlpFGColour.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,7 +157,6 @@
         private System.Windows.Forms.Panel panel1;
         private Eyedropper.EyedropColorPicker eyedropperColorPicker;
         private TileIconifier.Controls.SkinnableButton btnColourPicker;
-        private System.Windows.Forms.Panel pnlFGColour;
         private TileIconifier.Controls.SkinnableRadioButton radFGDark;
         private TileIconifier.Controls.SkinnableCheckBox chkFGTxtEnabled;
         private System.Windows.Forms.Label lblFGText;
@@ -165,5 +165,6 @@
         private System.Windows.Forms.Label lblBGColour;
         private TileIconifier.Controls.SkinnableTextBox txtBGColour;
         private System.Windows.Forms.ColorDialog clrDialog;
+        private System.Windows.Forms.TableLayoutPanel tlpFGColour;
     }
 }
