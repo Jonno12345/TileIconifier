31ff01e236d591a29625c5be9de8facfdbedb3de
mcdenis <maxecole@hotmail.com>
2017-04-13 15:27:56 -0400

    Reworked layout of PannablePictureBoxPanel and added a PannablePictureBox to it (design only, code unchanged)

--- TileIconifier/Controls/IconifierPanel/PictureBox/PannablePictureBoxControlPanel.Designer.cs
+++ TileIconifier/Controls/IconifierPanel/PictureBox/PannablePictureBoxControlPanel.Designer.cs
@@ -38,7 +38,14 @@
             this.btnAlign = new TileIconifier.Controls.SkinnableButton();
             this.trkZoom = new TileIconifier.Controls.SkinnableTrackBar();
             this.lblPercent = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flpCommands = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlZoomTrack = new System.Windows.Forms.Panel();
+            this.panPct = new TileIconifier.Controls.IconifierPanel.PictureBox.PannablePictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.trkZoom)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flpCommands.SuspendLayout();
+            this.pnlZoomTrack.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnShrink
@@ -46,10 +53,10 @@
             this.btnShrink.BackgroundImage = global::TileIconifier.Properties.Resources.ZoomOut_12927;
             this.btnShrink.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnShrink.DisabledForeColor = System.Drawing.Color.Empty;
-            this.btnShrink.Location = new System.Drawing.Point(106, 30);
-            this.btnShrink.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.btnShrink.Location = new System.Drawing.Point(1, 31);
+            this.btnShrink.Margin = new System.Windows.Forms.Padding(1);
             this.btnShrink.Name = "btnShrink";
-            this.btnShrink.Size = new System.Drawing.Size(27, 28);
+            this.btnShrink.Size = new System.Drawing.Size(28, 28);
             this.btnShrink.TabIndex = 0;
             this.btnShrink.UseVisualStyleBackColor = true;
             this.btnShrink.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnShrink_MouseDown);
@@ -60,10 +67,10 @@
             this.btnEnlarge.BackgroundImage = global::TileIconifier.Properties.Resources.Zoom_5442;
             this.btnEnlarge.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnEnlarge.DisabledForeColor = System.Drawing.Color.Empty;
-            this.btnEnlarge.Location = new System.Drawing.Point(106, -1);
-            this.btnEnlarge.Margin = new System.Windows.Forms.Padding(3, 0, 0, 3);
+            this.btnEnlarge.Location = new System.Drawing.Point(1, 1);
+            this.btnEnlarge.Margin = new System.Windows.Forms.Padding(1);
             this.btnEnlarge.Name = "btnEnlarge";
-            this.btnEnlarge.Size = new System.Drawing.Size(27, 28);
+            this.btnEnlarge.Size = new System.Drawing.Size(28, 28);
             this.btnEnlarge.TabIndex = 1;
             this.btnEnlarge.UseVisualStyleBackColor = true;
             this.btnEnlarge.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnEnlarge_MouseDown);
@@ -74,10 +81,10 @@
             this.btnOpenImage.BackgroundImage = global::TileIconifier.Properties.Resources.OpenComparisonResult_9697;
             this.btnOpenImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnOpenImage.DisabledForeColor = System.Drawing.Color.Empty;
-            this.btnOpenImage.Location = new System.Drawing.Point(106, 123);
-            this.btnOpenImage.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.btnOpenImage.Location = new System.Drawing.Point(1, 121);
+            this.btnOpenImage.Margin = new System.Windows.Forms.Padding(1);
             this.btnOpenImage.Name = "btnOpenImage";
-            this.btnOpenImage.Size = new System.Drawing.Size(27, 28);
+            this.btnOpenImage.Size = new System.Drawing.Size(28, 28);
             this.btnOpenImage.TabIndex = 2;
             this.btnOpenImage.UseVisualStyleBackColor = true;
             this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
@@ -87,10 +94,10 @@
             this.btnReset.BackgroundImage = global::TileIconifier.Properties.Resources.ZoomToFit;
             this.btnReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnReset.DisabledForeColor = System.Drawing.Color.Empty;
-            this.btnReset.Location = new System.Drawing.Point(106, 61);
-            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.btnReset.Location = new System.Drawing.Point(1, 61);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(1);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(27, 28);
+            this.btnReset.Size = new System.Drawing.Size(28, 28);
             this.btnReset.TabIndex = 3;
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
@@ -110,21 +117,22 @@
             this.btnAlign.BackgroundImage = global::TileIconifier.Properties.Resources.Translate;
             this.btnAlign.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnAlign.DisabledForeColor = System.Drawing.Color.Empty;
-            this.btnAlign.Location = new System.Drawing.Point(106, 92);
-            this.btnAlign.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.btnAlign.Location = new System.Drawing.Point(1, 91);
+            this.btnAlign.Margin = new System.Windows.Forms.Padding(1);
             this.btnAlign.Name = "btnAlign";
-            this.btnAlign.Size = new System.Drawing.Size(27, 28);
+            this.btnAlign.Size = new System.Drawing.Size(28, 28);
             this.btnAlign.TabIndex = 4;
             this.btnAlign.UseVisualStyleBackColor = true;
             this.btnAlign.Click += new System.EventHandler(this.btnAlign_Click);
             // 
             // trkZoom
             // 
-            this.trkZoom.Location = new System.Drawing.Point(0, 106);
+            this.trkZoom.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trkZoom.Location = new System.Drawing.Point(0, 0);
             this.trkZoom.Maximum = 100;
             this.trkZoom.Minimum = 1;
             this.trkZoom.Name = "trkZoom";
-            this.trkZoom.Size = new System.Drawing.Size(100, 45);
+            this.trkZoom.Size = new System.Drawing.Size(91, 45);
             this.trkZoom.TabIndex = 5;
             this.trkZoom.TickStyle = System.Windows.Forms.TickStyle.None;
             this.trkZoom.Value = 1;
@@ -132,31 +140,87 @@
             // 
             // lblPercent
             // 
-            this.lblPercent.Location = new System.Drawing.Point(25, 131);
+            this.lblPercent.Location = new System.Drawing.Point(23, 23);
             this.lblPercent.Name = "lblPercent";
             this.lblPercent.Size = new System.Drawing.Size(50, 13);
             this.lblPercent.TabIndex = 6;
             this.lblPercent.Text = "xxx%";
             this.lblPercent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.flpCommands, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pnlZoomTrack, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panPct, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(133, 154);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flpCommands
+            // 
+            this.flpCommands.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.flpCommands.AutoSize = true;
+            this.flpCommands.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flpCommands.Controls.Add(this.btnEnlarge);
+            this.flpCommands.Controls.Add(this.btnShrink);
+            this.flpCommands.Controls.Add(this.btnReset);
+            this.flpCommands.Controls.Add(this.btnAlign);
+            this.flpCommands.Controls.Add(this.btnOpenImage);
+            this.flpCommands.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flpCommands.Location = new System.Drawing.Point(100, 3);
+            this.flpCommands.Name = "flpCommands";
+            this.tableLayoutPanel1.SetRowSpan(this.flpCommands, 2);
+            this.flpCommands.Size = new System.Drawing.Size(30, 148);
+            this.flpCommands.TabIndex = 0;
+            this.flpCommands.WrapContents = false;
+            // 
+            // pnlZoomTrack
+            // 
+            this.pnlZoomTrack.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlZoomTrack.AutoSize = true;
+            this.pnlZoomTrack.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlZoomTrack.Controls.Add(this.lblPercent);
+            this.pnlZoomTrack.Controls.Add(this.trkZoom);
+            this.pnlZoomTrack.Location = new System.Drawing.Point(3, 106);
+            this.pnlZoomTrack.Name = "pnlZoomTrack";
+            this.pnlZoomTrack.Size = new System.Drawing.Size(91, 45);
+            this.pnlZoomTrack.TabIndex = 1;
+            // 
+            // panPct
+            // 
+            this.panPct.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panPct.AssociatedSize = new System.Drawing.Size(0, 0);
+            this.panPct.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panPct.Location = new System.Drawing.Point(23, 26);
+            this.panPct.Margin = new System.Windows.Forms.Padding(0);
+            this.panPct.Name = "panPct";
+            this.panPct.Size = new System.Drawing.Size(50, 50);
+            this.panPct.TabIndex = 2;
+            // 
             // PannablePictureBoxControlPanel
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.Controls.Add(this.lblPercent);
-            this.Controls.Add(this.trkZoom);
-            this.Controls.Add(this.btnAlign);
-            this.Controls.Add(this.btnReset);
-            this.Controls.Add(this.btnOpenImage);
-            this.Controls.Add(this.btnEnlarge);
-            this.Controls.Add(this.btnShrink);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "PannablePictureBoxControlPanel";
-            this.Size = new System.Drawing.Size(133, 154);
+            this.Size = new System.Drawing.Size(136, 157);
             this.Load += new System.EventHandler(this.PannablePictureBoxControlPanel_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trkZoom)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flpCommands.ResumeLayout(false);
+            this.pnlZoomTrack.ResumeLayout(false);
+            this.pnlZoomTrack.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -171,5 +235,9 @@
         private TileIconifier.Controls.SkinnableButton btnAlign;
         private TileIconifier.Controls.SkinnableTrackBar trkZoom;
         private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flpCommands;
+        private System.Windows.Forms.Panel pnlZoomTrack;
+        private PannablePictureBox panPct;
     }
 }
